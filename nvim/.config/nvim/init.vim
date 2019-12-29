3e036d28bd8a825cb3ba0849a46fd26a02718e77
Dominic Henry <dominichenrywork@hotmail.com>
2019-12-29 14:43:03 -0500

    Updating dotfiles - stow method

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,116 @@
+" Plugins
+call plug#begin()
+
+" Auto Completion(Coc)
+" Plug 'neoclide/coc.nvim', { 'branch' : 'release' }
+Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+Plug 'zchee/deoplete-jedi' " Python 
+Plug 'wokalski/autocomplete-flow' " Javascript
+Plug 'lvht/phpcd.vim', { 'for': 'php', 'do': 'composer install' } " PHP
+
+
+" Snippet Autocompletion supplement
+Plug 'Shougo/neosnippet'
+Plug 'Shougo/neosnippet-snippets' " Language Snippet Repo
+
+
+" Nerd Plugins
+Plug 'scrooloose/nerdtree' " File Explorer
+Plug 'scrooloose/nerdcommenter'    
+Plug 'xuyuanp/nerdtree-git-plugin' 
+
+" Fuzzy Finder
+Plug 'ctrlpvim/ctrlp.vim'
+
+" Git 
+Plug 'airblade/vim-gitgutter'
+
+" Pandoc Markdown Preview
+Plug 'conornewton/vim-pandoc-markdown-preview'
+
+" Neovim bottom bar 
+Plug 'bling/vim-airline'
+
+" Surround 
+Plug 'tpope/vim-surround'
+
+
+" MISC Plugins
+Plug 'Raimondi/delimitMate' " Autocompletes brackets, quotes and parenthesis
+
+call plug#end()
+
+" Leader key 
+let mapleader = ","
+
+" Disable autocommenting
+autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+
+" Indentation
+set tabstop=4 
+set shiftwidth=4
+set expandtab
+set autoindent
+set smartindent
+
+" Line numbers
+set number
+set relativenumber
+
+" Autocompletion (doeplete)
+let g:deoplete#enable_at_startup = 1
+
+" PHP deoplete 
+let g:deoplete#ignore_sources = get(g:, 'deoplete#ignore_sources', {})
+let g:deoplete#ignore_sources.php = ['omni']
+
+" Language Snippets (neosnippet)
+let g:neosnippet#enable_completed_snippet = 1
+
+" Note: It must be "imap" and "smap".  It uses <Plug> mappings.
+imap <C-k>     <Plug>(neosnippet_expand_or_jump)
+smap <C-k>     <Plug>(neosnippet_expand_or_jump)
+xmap <C-k>     <Plug>(neosnippet_expand_target)
+
+" SuperTab like snippets behavior.
+" Note: It must be "imap" and "smap".  It uses <Plug> mappings.
+"imap <expr><TAB>
+" \ pumvisible() ? "\<C-n>" :
+" \ neosnippet#expandable_or_jumpable() ?
+" \    "\<Plug>(neosnippet_expand_or_jump)" : "\<TAB>"
+smap <expr><TAB> neosnippet#expandable_or_jumpable() ?
+\ "\<Plug>(neosnippet_expand_or_jump)" : "\<TAB>"
+
+" For conceal markers.
+if has('conceal')
+  set conceallevel=2 concealcursor=niv
+endif
+
+" Language Snippets (end)
+
+" NERD STUFF 
+" NerdTree Config
+noremap <leader>n :NERDTreeToggle<CR>
+
+" NerdTree git plugin
+"let g:NERDTreeIndicatorMapCustom = {
+    ""Modified"  : "✹",
+    ""Staged"    : "✚",
+    ""Untracked" : "✭",
+    ""Renamed"   : "➜",
+    ""Unmerged"  : "═",
+    ""Deleted"   : "✖",
+    ""Dirty"     : "✗",
+    ""Clean"     : "✔︎",
+    "'Ignored'   : '☒',
+    ""Unknown"   : "?"
+"}
+
+" NerdCommenter
+" [count]<leader>c<space> NerdCommenter Toggle
+" <leader>cA Append comment to current line
+" <leader>ca Switches to alternative commenting delimiters
+
+
+" Pandoc Markdown Preview 
+let g:md_pdf_viewer="zathura" " Default Pdf previewer
---
+++ qtile/.config/qtile/config.py
@@ -0,0 +1,124 @@
+from libqtile import bar, hook, layout, widget
+from libqtile.command import lazy
+from libqtile.config import Click, Drag, Group, Key, Screen
+
+from libqtile.bar import Bar
+from libqtile.widget import Spacer
+from powerline.bindings.qtile.widget import PowerlineTextBox
+
+wmname = 'qtile'
+mod = 'mod1'
+
+# Key bindings
+keys = [
+    # Window manager controls
+    Key([mod, 'control'], 'r', lazy.restart()),
+    Key([mod, 'control'], 'q', lazy.shutdown()),
+    Key([mod], 'Return', lazy.spawn('urxvt')),
+    Key([mod, 'shift'], 'q',      lazy.window.kill()),
+
+    Key([mod], 'Left', lazy.screen.prevgroup()),
+    Key([mod], 'Right', lazy.screen.nextgroup()),
+
+    # Layout modification
+    Key([mod, 'control'], 'space', lazy.window.toggle_floating()),
+
+    # Switch between windows in current stack pane
+    Key([mod], 'j', lazy.layout.down()),
+    Key([mod], 'k', lazy.layout.up()),
+    Key([mod], 'h', lazy.layout.right()),
+    Key([mod], 'l', lazy.layout.left()),
+
+    # Move windows up or down in current stack
+    Key([mod, 'control'], 'j', lazy.layout.shuffle_down()),
+    Key([mod, 'control'], 'k', lazy.layout.shuffle_up()),
+    Key([mod, 'control'], 'h', lazy.layout.shuffle_right()),
+    Key([mod, 'control'], 'l', lazy.layout.shuffle_left()),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], 'space', lazy.layout.next()),
+
+    # Toggle between different layouts as defined below
+    Key([mod], 'Tab', lazy.next_layout()),
+    Key([mod], 'm', lazy.to_layout_index(0)),
+
+    # ROFI program spawner
+    Key([mod], 'r', lazy.spawn("rofi -show run")),
+]
+
+# Mouse bindings and options
+mouse = (
+    Drag([mod], 'Button1', lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+)
+
+bring_front_click = True
+cursor_warp = False
+follow_mouse_focus = True
+
+# Groups
+groups = [
+    Group('a'),
+    Group('s'),
+    Group('d'),
+    Group('f'),
+    Group('u'),
+    Group('i'),
+    Group('o'),
+    Group('p'),
+]
+for i in groups:
+    # mod + letter of group = switch to group
+    keys.append(Key([mod], i.name, lazy.group[i.name].toscreen()))
+
+    # mod + shift + letter of group = switch to & move focused window to group
+    keys.append(Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)))
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+
+# Layouts
+layouts = [
+    layout.Max(),
+    layout.Stack(num_stacks=2),
+    layout.Tile(),
+    layout.Columns(),
+    layout.RatioTile(),
+    layout.Matrix(),
+]
+
+floating_layout = layout.Floating()
+
+# Screens and widget options
+screens = [
+    Screen(
+        top=bar.Bar(
+            widgets=[
+                widget.GroupBox(
+                    highlight_method='block',
+                    inactive='999999'
+                ),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Systray(),
+                widget.Clock(format='%a %d %b %I:%M %p'),
+            ],
+            size=30,
+            background=['222222', '111111'],
+        ),
+    ),
+]
+
+widget_defaults = dict(
+    font='Arial',
+    fontsize=15,
+)
+
+auto_fullscreen = True
+
+
+def main(qtile):
+    ''' This function is called when Qtile starts. '''
+    lazy.spawn('redshift')
